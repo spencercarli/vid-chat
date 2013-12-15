6b319af48a13848b290460bbe7e6400b993ffc18
Spencer Carli <spencercarli@gmail.com>
2013-12-14 22:44:06 -0500

    Setup publications

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -1,18 +1,7 @@
-var videos = [
-	{
-    "title": "Porsche 918 Spyder driven - is it better than a Bugatti Veyron?",
-    "url": "www.youtube.com/embed/D9vBKEGjytE",
-    "id": "a"
-	},
-	{
-    "title": "Jaguar C-X75 hypercar - LaFerrari and McLaren P1 rival",
-    "url": "www.youtube.com/embed/CspcnZ1IBs8",
-    "id": "b"
-	}
-];
+Meteor.subscribe("videos");
 
 Template.dashboard.helpers({
   video: function(){
-    return videos;
+    return Videos.find();
   }
 });
---
+++ server/fixtures.js
@@ -0,0 +1,11 @@
+if (Videos.find().count() === 0){
+	Videos.insert({
+		title: "Porsche 918 Spyder driven - is it better than a Bugatti Veyron?",
+		url: "www.youtube.com/embed/D9vBKEGjytE"
+	});
+
+	Videos.insert({
+		title: "Jaguar C-X75 hypercar - LaFerrari and McLaren P1 rival",
+		url: "www.youtube.com/embed/CspcnZ1IBs8"
+	});
+}
