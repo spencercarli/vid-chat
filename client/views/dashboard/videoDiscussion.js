e689784286e2f3679030716321fdbf9b3fcd524b
Spencer Carli <spencercarli@gmail.com>
2013-12-15 00:01:00 -0500

    Add ability to comment on a video

--- client/views/dashboard/videoDiscussion.js
+++ client/views/dashboard/videoDiscussion.js
@@ -4,4 +4,19 @@ Template.videoDiscussion.helpers({
 	comments: function(){
 		return Comments.find({videoId: this._id});
 	}
-})
+});
+
+Template.videoDiscussion.events({
+	'click #submit': function(e){
+		e.preventDefault();
+
+		var message = $('#message').val()
+
+		var comment = {
+			videoId: this._id,
+			message: message
+		}
+
+		Comments.insert(comment);
+	}
+});
